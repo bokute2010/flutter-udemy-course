df8b56cea3028cb0b150b44073e6e5d001d740de
bokute2010 <lequanvt299@gmail.com>
2024-06-05 01:40:01 +0700

    103. Using Lists Inside Of Lists

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,5 +1,8 @@
+import 'package:expense_tracker/expense_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/widgets.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
@@ -33,9 +36,12 @@ class _ExpensesState extends State<Expenses> {
   ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [Text('The chart'), Text('Expanse List...')],
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpenseList(expenses: _registeredExpenses))
+        ],
       ),
     );
   }
